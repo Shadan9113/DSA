f7a12ec7e8c0fd40c67fb6bfc6f5a82eef234b28
Shadan9113 <mdshadanzaheer@gmail.com>
2025-07-09 13:00:34 +0530

    find min element in an array

--- array/minElement.js
+++ array/minElement.js
@@ -2,7 +2,7 @@ const arr = [1,2,3,4];
 
 function minElement(arr){
          let min = arr[0];
-    for(let i=0; i<arr.length; i++){
+    for(let i=1; i<arr.length; i++){
    
         if(arr[i] < min){
             min = arr[i]
