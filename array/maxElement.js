5c8bf3357fd8c3ba619c9de2528b8be2ccbf35b4
Shadan9113 <mdshadanzaheer@gmail.com>
2025-07-09 13:03:00 +0530

    find max element in an array

--- array/maxElement.js
+++ array/maxElement.js
@@ -2,7 +2,7 @@ const arr = [1,2,3,4];
 
 function maxElement(arr){
          let max = arr[0];
-    for(let i=0; i<arr.length; i++){
+    for(let i=1; i<arr.length; i++){
    
         if(arr[i] > max){
             max = arr[i]
