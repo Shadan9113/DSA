2c5741cbb51cfab05799f9a915d2149378be9740
Shadan9113 <mdshadanzaheer@gmail.com>
2025-07-09 12:57:13 +0530

    find min element

---
+++ array/minElement.js
@@ -0,0 +1,14 @@
+const arr = [1,2,3,4];
+
+function minElement(arr){
+         let min = arr[0];
+    for(let i=0; i<arr.length; i++){
+   
+        if(arr[i] < min){
+            min = arr[i]
+        }
+          
+    }
+  return min
+}
+console.log(minElement(arr))
